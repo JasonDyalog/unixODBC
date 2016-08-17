d65c2ad20ed06b2965c1b8e3aaa95bb9e37a7788
Jason Rivers <jason@jasonrivers.co.uk>
2016-08-17 17:25:04 +0100

    add byacc

--- dbuild.sh
+++ dbuild.sh
@@ -4,7 +4,7 @@ set -e
 
 ### Setup the system
 apt-get update
-apt-get install -y build-essential automake autoconf libtool
+apt-get install -y build-essential automake autoconf libtool byacc
 
 cd /unixODBC
 
